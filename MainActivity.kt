99820d9bbb8da17d5d181ba3ef3b4340c1811d56
arifuljoy1913-star <arifuljoy1913@gmail.com>
2026-06-12 01:35:44 +0600

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,10 @@
+package net.one.app
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // মোবাইল ডুয়াল UI ৭টি ফোল্ডার কঙ্কাল লোডার
+    }
+}
