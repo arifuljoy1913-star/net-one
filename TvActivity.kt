b8118110d7135c6e35484a3dd12ae4784241b33c
arifuljoy1913-star <arifuljoy1913@gmail.com>
2026-06-12 01:37:28 +0600

    Add files via upload

---
+++ TvActivity.kt
@@ -0,0 +1,10 @@
+package net.one.app
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TvActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // টিভি লিনব্যাক ভিডিও ইঞ্জিন কঙ্কাল
+    }
+}
